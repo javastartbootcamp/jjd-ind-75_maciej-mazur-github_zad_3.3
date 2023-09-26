064d059db0960ccde1ce94a001a061cc23d53b63
Maciej Mazur <maciej.mazur.mailbox@gmail.com>
2023-09-26 20:24:16 +0200

    Solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
+
+        System.out.println("Czy liczba 5 jest parzysta? " + calculator.isEven(5));
+        System.out.println("Czy liczba 4 jest nieparzysta? " + calculator.isOdd(4));
+        System.out.println("===========================");
+        System.out.println("Pole koła o promieniu 5.25: " + calculator.circleField(5.25));
+        System.out.println("Kwadrat liczby 10 to: " + calculator.power(10));
     }
 }
